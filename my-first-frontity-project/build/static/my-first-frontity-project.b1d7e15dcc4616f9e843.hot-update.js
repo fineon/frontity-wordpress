b452145cae8f705e85847acb609172aab50ac7ba
Ian <ian@heythereian.com>
2021-03-03 23:03:26 -0800

    created a new branch to create my own theme package

---
+++ my-first-frontity-project/build/static/my-first-frontity-project.b1d7e15dcc4616f9e843.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("my-first-frontity-project",{
+
+/***/ "./packages/my-first-theme/src/index.js":
+/*!**********************************************!*\
+  !*** ./packages/my-first-theme/src/index.js ***!
+  \**********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _emotion_react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @emotion/react/jsx-runtime */ \"./node_modules/@emotion/react/jsx-runtime/dist/emotion-react-jsx-runtime.browser.esm.js\");\nconst Root=()=>{return Object(_emotion_react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__[\"jsx\"])(_emotion_react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__[\"Fragment\"],{children:Object(_emotion_react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__[\"jsx\"])(\"h1\",{children:\"Hello It's Ian\"})});};/* harmony default export */ __webpack_exports__[\"default\"] = ({name:\"my-first-theme\",roots:{myTheme:Root},state:{myTheme:{}},actions:{myTheme:{}}});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWNrYWdlcy9teS1maXJzdC10aGVtZS9zcmMvaW5kZXguanM/MmUwZSJdLCJuYW1lcyI6WyJSb290IiwibmFtZSIsInJvb3RzIiwibXlUaGVtZSIsInN0YXRlIiwiYWN0aW9ucyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxLQUFNQSxLQUFJLENBQUcsSUFBTSxDQUNqQixNQUNFLHNKQUNDLHdHQURELEVBREYsQ0FLRCxDQU5ELENBUWUsZ0VBQ2JDLElBQUksQ0FBRSxnQkFETyxDQUViQyxLQUFLLENBQUUsQ0FDTEMsT0FBTyxDQUFFSCxJQURKLENBRk0sQ0FLYkksS0FBSyxDQUFFLENBQ0xELE9BQU8sQ0FBRSxFQURKLENBTE0sQ0FRYkUsT0FBTyxDQUFFLENBQ1BGLE9BQU8sQ0FBRSxFQURGLENBUkksQ0FBZiIsImZpbGUiOiIuL3BhY2thZ2VzL215LWZpcnN0LXRoZW1lL3NyYy9pbmRleC5qcy5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnN0IFJvb3QgPSAoKSA9PiB7XG4gIHJldHVybiAoXG4gICAgPD5cbiAgICAgPGgxPkhlbGxvIEl0J3MgSWFuPC9oMT5cbiAgICA8Lz5cbiAgKTtcbn07XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgbmFtZTogXCJteS1maXJzdC10aGVtZVwiLFxuICByb290czoge1xuICAgIG15VGhlbWU6IFJvb3RcbiAgfSxcbiAgc3RhdGU6IHtcbiAgICBteVRoZW1lOiB7fVxuICB9LFxuICBhY3Rpb25zOiB7XG4gICAgbXlUaGVtZToge31cbiAgfVxufTtcbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./packages/my-first-theme/src/index.js\n");
+
+/***/ })
+
+})
