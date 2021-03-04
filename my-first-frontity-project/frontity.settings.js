c30d68a52796ea68729d8d29f6dd87ff724b54a6
Ian <ian@heythereian.com>
2021-03-03 22:51:08 -0800

    changed frontity title in state, inspecting frontity components

--- my-first-frontity-project/frontity.settings.js
+++ my-first-frontity-project/frontity.settings.js
@@ -3,7 +3,7 @@ const settings = {
   "state": {
     "frontity": {
       "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
+      "title": "It's Ian",
       "description": "WordPress installation for Frontity development"
     }
   },
